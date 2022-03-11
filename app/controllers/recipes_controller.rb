5ee752d2b9d6cd28e835594e9ac5451cc71dce8e
TheDoctorQ <ilias.schwarz42@gmail.com>
2022-03-11 10:52:30 -0700

    adds recipe controller & index

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,11 @@
 class RecipesController < ApplicationController
+
+  def index
+    #show ALL the recipes
+    recipes = Recipe.all
+    render json: recipes.as_json
+  end
+
+
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get "/recipes" => "recipe#index"
+
+  get "/recipe/:id" => "recipe#show"
 end
